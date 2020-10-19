4a43478c729efd32cf08cf34ab2954ae765b248f
Divine Ugizwenayo <ugizwenayodiny@gmail.com>
2020-10-19 08:24:50 -1000

    -build

---
+++ build/precache-manifest.03f5d35c536374e86071f1a41265894d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f1b6cddf6958b1cd2fe418fa6a60e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feeeeda401ff65aa5d3d",
+    "url": "/static/css/2.fe7cd3c1.chunk.css"
+  },
+  {
+    "revision": "9a74ef7fdc35bf672dda",
+    "url": "/static/css/main.d6602e9f.chunk.css"
+  },
+  {
+    "revision": "feeeeda401ff65aa5d3d",
+    "url": "/static/js/2.938f9c8b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.938f9c8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a74ef7fdc35bf672dda",
+    "url": "/static/js/main.2f3a0ebc.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
